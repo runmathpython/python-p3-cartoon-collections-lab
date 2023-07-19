62f4904c749d27647448008d9ddab34b68aa9ce3
Seong Kim <runmath@gmail.com>
2023-07-18 22:39:39 -0600

    test done

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,22 @@
-def roll_call_dwarves():
-    pass
+cheeses = ["cheddar", "gouda", "camebert"]
 
-def summon_captain_planet():
-    pass
+def roll_call_dwarves(dwarves):
+    i = 1
+    for dwarf in dwarves:
+        print(f'{i}. {dwarf}')
+        i += 1
 
-def long_planeteer_calls():
-    pass
+def summon_captain_planet(planeteer_calls):
+    return [f'{planeteer_call.title()}!' for planeteer_call in planeteer_calls]
 
-def find_the_cheese():
-    pass
+def long_planeteer_calls(calls_list):
+    for call in calls_list:
+        if len(call) > 4:
+            return True
+    return False
+
+def find_the_cheese(foods_list):
+    for food in foods_list:
+        if food in cheeses:
+            return food
+    return None
